286ed5555e9ea4c6e1846d2086f12ab865f1872d
Edin Pjević <ep50655@fer.hr>
2021-10-12 10:59:35 +0200

    Added ARIMA forecasting for prepared data

--- forecast_prepared_data.py
+++ forecast_prepared_data.py
@@ -1,16 +1,33 @@
 import pandas as pd
 from statsmodels.tsa.api import Holt
+from statsmodels.tsa.arima.model import ARIMA
 
 SOURCE_PATH = 'prepared_data\\ALPHS_FC_Prepared_15_mth.csv'
-SHEET_NAME = 'FC 3 mth horizon'
-
 MAT_GROUP = "ALPHS"
+
+# holt or arima
+MODEL = "arima"
 
 WINDOW_SIZE = 12
 FORECAST_SIZE = 3
 SMOOTH_LVL = .7
 SMOOTH_SLOPE = 0.001
 DAMPED_TREND = 0.98
+
+AUTO_REGRESSION = 1
+DIFFERENCING = 1
+MOVING_AVREAGE = 1
+
+
+def arima_forecasting_3_mnt(unit_data):
+    train = unit_data.iloc[:WINDOW_SIZE]
+    train = train.asfreq('MS')
+    train = train.astype('int')
+    model = ARIMA(train.values, order=(AUTO_REGRESSION, DIFFERENCING, MOVING_AVREAGE))
+    model_fit = model.fit()
+    predictions = model_fit.forecast(3)
+    print(model_fit.summary())
+    return predictions
 
 
 def holts_dampening_forecasting_3_mnt(unit_data):
@@ -36,9 +53,12 @@ def main():
     filtered_data = data[data["Material Group"] == "ALPHS"].values[0][1:]
     new_index = pd.to_datetime(data.columns[1:])
     unit_data = pd.Series(filtered_data, new_index)
-    predictions = holts_dampening_forecasting_3_mnt(unit_data)
+    if MODEL == 'arima':
+        predictions = arima_forecasting_3_mnt(unit_data)
+    else:
+        predictions = holts_dampening_forecasting_3_mnt(unit_data)
     print()
-    print(f"Predictions: {predictions}")
+    print(f"Predictions: \n{predictions}")
     test = unit_data.iloc[WINDOW_SIZE:]
     test.index = pd.to_datetime(test.index)
     rel_error = calculate_relative_error(predictions, test.values)
