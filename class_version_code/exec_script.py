aba1f0005c16c104e77996832d9261539ac8b057
Edin Pjević <ep50655@fer.hr>
2021-11-29 13:30:01 +0100

    Implemented the grundfos approach

--- class_version_code/exec_script.py
+++ class_version_code/exec_script.py
@@ -1,6 +1,7 @@
 import warnings
 import pandas as pd
 import os
+import statistics as stat
 
 from statsmodels.tools.sm_exceptions import ConvergenceWarning
 
@@ -20,14 +21,15 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
 
 # path from which we extract product group data
-SOURCE_PATH = '..\\prepared_data\\DBS_SOLOL_10Y_Prepared.csv'
+SOURCE_PATH = '..\\prepared_data\\DBS_SUPM3_10Y_Prepared.csv'
 
 # Choose whether you want to forecast for the whole product group or just for material group
 # Choices:
 #   "PG Global" - for forecasting on sum of material groups
 #   "PG" - for forecasting on all material groups and then summing
 #   material group name (e.g. "ALPHS") - for forecasting on some material group
-MAT_GROUP = "PG Global"
+#   "Grundfos" - for Grundfos forecasting
+MAT_GROUP = "Grundfos"
 # Name of the column in which the material group names are
 MAT_COL_NAME = "MD Material Group"
 # Choose method or model to use for forecasting
@@ -39,13 +41,13 @@ MAT_COL_NAME = "MD Material Group"
 MODEL = "all"
 
 # parameters for forecasting
-WINDOW_SIZE = 91
-FORECAST_SIZE = 18
+TRAINING_SIZE = 91
+FORECAST_SIZE = 8
 
 # parameters for Holts method
-SMOOTH_LVL = .2
-SMOOTH_SLOPE = .2
-DAMPED_TREND = .7
+SMOOTH_LVL = .6
+SMOOTH_SLOPE = .3
+DAMPED_TREND = .8
 
 # parameters for the ARIMA model
 AUTO_REGRESSION = 1
@@ -54,7 +56,7 @@ MOVING_AVREAGE = 1
 
 # parameters for the LSTM model
 NUM_OF_EPOCH = 100
-NEURON_SIZE = 8
+OUTPUT_SIZE = 8
 LOOK_BACK = 6
 
 # how much data-points to show on plot before forecasting
@@ -62,6 +64,60 @@ PLOT_SIZE = 15
 
 # do we print the method and model summaries as well as their progression
 DO_PRINT = False
+
+
+def grundfos_forecasting(prepared_data, forecasting_class):
+    simple_exp_model = Holts(TRAINING_SIZE, FORECAST_SIZE, 0.2, 0, 0, is_damped=False,
+                        do_print=DO_PRINT)
+    product_group_exp_smoothing_model = forecasting_class
+    total_mg_predictions = []
+
+    for material_group in prepared_data[MAT_COL_NAME]:
+        filtered_data = prepared_data[prepared_data[MAT_COL_NAME] == material_group].values[0][1:]
+        new_index = pd.to_datetime(prepared_data.columns[1:])
+        unit_data = pd.Series(filtered_data, new_index)
+        simple_exp_model.forecast(unit_data)
+        mg_predictions = simple_exp_model.predictions
+        total_mg_predictions.append(mg_predictions)
+
+    pg_predictions = []
+    for i in range(len(total_mg_predictions[0])):
+        temp_sum = 0.0
+        for j in range(len(prepared_data[MAT_COL_NAME])):
+            temp_sum += total_mg_predictions[j][i]
+        pg_predictions.append(temp_sum)
+
+    total_mg_ratio_list = []
+    for i in range(len(total_mg_predictions)):
+        mg_ratio_list = []
+        for j in range(len(total_mg_predictions[i])):
+            mg_ratio_list.append(total_mg_predictions[i][j]/pg_predictions[j])
+        total_mg_ratio_list.append(mg_ratio_list)
+
+    product_group_data = combine_material_groups(prepared_data)
+    product_group_exp_smoothing_model.forecast(product_group_data)
+    product_group_predictions = product_group_exp_smoothing_model.predictions
+
+    total_mg_final_predictions = []
+    for i in range(len(total_mg_ratio_list)):
+        mg_final_pred = []
+        for j in range(len(total_mg_ratio_list[i])):
+            mg_final_pred.append(total_mg_ratio_list[i][j] * product_group_predictions[j])
+        total_mg_final_predictions.append(mg_final_pred)
+
+    total_relative_errors = []
+    for mg in total_mg_final_predictions:
+        product_group_exp_smoothing_model.predictions = mg
+        product_group_exp_smoothing_model.calculate_relative_error()
+        total_relative_errors.append(product_group_exp_smoothing_model.rel_error)
+
+    final_relative_error = stat.mean(total_relative_errors)
+
+    total_pg_pred = [0.0] * len(total_mg_final_predictions[0])
+    for mg in total_mg_final_predictions:
+        total_pg_pred = [x + y for x, y in zip(total_pg_pred, mg)]
+    product_group_exp_smoothing_model.predictions = total_pg_pred
+    return total_pg_pred
 
 
 def total_print(holts_model, arima_model, lstm_model, predictions, unit_data):
@@ -171,10 +227,10 @@ def main():
     # read the prepared data from the cvs file
     data = pd.read_csv(SOURCE_PATH, header=0)
     # initialize the forecasting methods/models
-    arima_model = Arima(WINDOW_SIZE, FORECAST_SIZE, AUTO_REGRESSION, DIFFERENCING, MOVING_AVREAGE, do_print=DO_PRINT)
-    holts_model = Holts(WINDOW_SIZE, FORECAST_SIZE, SMOOTH_LVL, SMOOTH_SLOPE, DAMPED_TREND, is_damped=True,
+    arima_model = Arima(TRAINING_SIZE, FORECAST_SIZE, AUTO_REGRESSION, DIFFERENCING, MOVING_AVREAGE, do_print=DO_PRINT)
+    holts_model = Holts(TRAINING_SIZE, FORECAST_SIZE, SMOOTH_LVL, SMOOTH_SLOPE, DAMPED_TREND, is_damped=True,
                         do_print=DO_PRINT)
-    lstm_model = Lstm(WINDOW_SIZE, FORECAST_SIZE, NUM_OF_EPOCH, NEURON_SIZE, LOOK_BACK, do_print=DO_PRINT)
+    lstm_model = Lstm(TRAINING_SIZE, FORECAST_SIZE, NUM_OF_EPOCH, OUTPUT_SIZE, LOOK_BACK, do_print=DO_PRINT)
     # forecast on the summed up product group data
     if MAT_GROUP == "PG Global":
         unit_data = combine_material_groups(data)
@@ -193,6 +249,10 @@ def main():
         predictions = total_predictions
         unit_data = combine_material_groups(data)
         total_print(holts_model, arima_model, lstm_model, predictions, unit_data)
+    elif MAT_GROUP == 'Grundfos':
+        predictions = [grundfos_forecasting(data, arima_model), grundfos_forecasting(data, holts_model),
+                       grundfos_forecasting(data, lstm_model)]
+        unit_data = combine_material_groups(data)
     # forecast for a single material group
     else:
         filtered_data = data[data[MAT_COL_NAME] == MAT_GROUP].values[0][1:]
